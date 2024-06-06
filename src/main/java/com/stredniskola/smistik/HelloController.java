138c75b4704187218129f399402ba45fd33ea15e
smistik <smistik@stredniskola.com>
2024-06-06 12:46:20 +0200

    Implementace změny barvy auta pomocí ColorPickeru

--- src/main/java/com/stredniskola/smistik/HelloController.java
+++ src/main/java/com/stredniskola/smistik/HelloController.java
@@ -1,14 +1,19 @@
 package com.stredniskola.smistik;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Polygon;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private Polygon auto;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ColorPicker barva;
+
+
+    public void zmenBarvu(ActionEvent event) {
+        auto.setFill(barva.getValue());
     }
 }
